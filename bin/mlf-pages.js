318ff3afc52abd1e0300c02fb996e2dcdc63a79c
malinfeng <80527573@qq.com>
2020-11-01 16:27:50 +0800

    创建爱你好了

---
+++ bin/mlf-pages.js
@@ -0,0 +1,7 @@
+#!/usr/local/bin/node 
+process.argv.push("--cwd")
+process.argv.push(process.cwd())
+process.argv.push("--gulpfile")
+process.argv.push(require.resolve(".."))
+require('gulp/bin/gulp')
+console.log(process.argv)
